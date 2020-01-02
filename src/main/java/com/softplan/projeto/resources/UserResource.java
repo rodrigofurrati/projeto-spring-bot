252389dc06eb149b3ffb5df18420216aa959ff9c
Rodrigo Furrati <rodrigofurrati95@hotmail.com>
2020-01-02 16:56:40 -0300

    listando usuarios

---
+++ src/main/java/com/softplan/projeto/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.softplan.projeto.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.softplan.projeto.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Rodrigo Furrati", "rodrigofurrati95@hotmail.com", "51998868003", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
